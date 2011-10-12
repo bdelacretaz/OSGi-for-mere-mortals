b5f09780c0d2730fd8c773db38fa2e676bb97e74
Bertrand Delacretaz <bdelacretaz@codeconsult.ch>
2011-10-12 17:27:39 +0200

    Basic skeleton, runnable jar

---
+++ src/main/java/ch/x42/osgi/samples/osgi101/bootstrap/StartServer.java
@@ -0,0 +1,7 @@
+package ch.x42.osgi.samples.osgi101.bootstrap;
+
+public class StartServer {
+    public static void main(String [] args) {
+      System.err.println("StartServer starting, but does nothing for now");
+    }
+}
