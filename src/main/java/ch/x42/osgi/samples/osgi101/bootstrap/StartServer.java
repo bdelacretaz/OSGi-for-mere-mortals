e2e7c5e11b80a14652d125f3ff2b39fad3976598
Bertrand Delacretaz <bdelacretaz@codeconsult.ch>
2011-10-12 18:27:27 +0200

    Don't stop framework, can be done via felix shell now

--- src/main/java/ch/x42/osgi/samples/osgi101/bootstrap/StartServer.java
+++ src/main/java/ch/x42/osgi/samples/osgi101/bootstrap/StartServer.java
@@ -1,7 +1,30 @@
 package ch.x42.osgi.samples.osgi101.bootstrap;
 
+import java.io.File;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.launch.Framework;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StartServer {
-    public static void main(String [] args) {
-      System.err.println("StartServer starting, but does nothing for now");
+    
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+    void testBootstrap() throws Exception {
+        
+        final OsgiBootstrap osgi = new OsgiBootstrap();
+        final Framework framework = osgi.getFramework();
+        
+        log.info("Framework bundle: {} ({})", framework.getSymbolicName(), framework.getState());
+        osgi.installBundles(new File("target/bundles"));
+        for(Bundle b : framework.getBundleContext().getBundles()) {
+            log.info("Installed bundle: {} ({})", b.getSymbolicName(), b.getState());
+        }
+        osgi.waitForFrameworkAndQuit();
+    }
+    
+    public static void main(String [] args) throws Exception {
+        new StartServer().testBootstrap();
     }
 }
