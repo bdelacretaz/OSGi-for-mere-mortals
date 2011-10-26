7288ca199389b2ba1344724cc06fa9006ac4c512
Bertrand Delacrétaz <bdelacretaz@codeconsult.ch>
2011-10-26 17:37:02 +0200

    Simplistic DispatcherServlet implementation

--- core/src/main/java/ch/x42/osgi/samples/osgi101/core/DispatcherServlet.java
+++ core/src/main/java/ch/x42/osgi/samples/osgi101/core/DispatcherServlet.java
@@ -2,6 +2,7 @@ package ch.x42.osgi.samples.osgi101.core;
 
 import java.io.IOException;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +12,12 @@ import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
+import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,24 +32,57 @@ public class DispatcherServlet extends HttpServlet {
     
     @Reference
     private HttpService httpService;
+    
+    /** Keep track of Servlets registered as OSGi services,
+     *  so that we can dispatch requests to them.
+     */
+    private ServiceTracker servletServicesTracker;
+    
+    public static final String SERVLET_METHOD_PROP = "osgi101.servlet.method";
+    public static final String SERVLET_PATH_PROP = "osgi101.servlet.path";
+    
+    private BundleContext bundleContext;
 
     @Activate
     protected void activate(ComponentContext ctx) throws ServletException, NamespaceException {
+        bundleContext = ctx.getBundleContext();
         httpService.registerServlet(MOUNT_PATH, this, null, null);
+        servletServicesTracker = new ServiceTracker(ctx.getBundleContext(), Servlet.class.getName(), null);
+        servletServicesTracker.open();
         log.info("{} registered at {}", getClass().getSimpleName(), MOUNT_PATH);
     }
     
     @Deactivate
     protected void deactivate(ComponentContext ctx) {
         httpService.unregister(MOUNT_PATH);
+        servletServicesTracker.close();
+        servletServicesTracker = null;
+        bundleContext = null;
         log.info("{} unregistered from {}", getClass().getSimpleName(), MOUNT_PATH);
     }
     
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    protected void service(HttpServletRequest req, HttpServletResponse resp) 
     throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().write("This is " + getClass().getName());
-        resp.getWriter().flush();
+        // Select a servlet to dispatch to, based on its service properties
+        // TODO might implement some caching, not needed for this simple example
+        final String method = req.getMethod();
+        final String path = req.getPathInfo();
+        ServiceReference selectedService = null;
+        for(ServiceReference  ref : servletServicesTracker.getServiceReferences()) {
+            final String serviceMethod = (String)ref.getProperty(SERVLET_METHOD_PROP);
+            final String servicePath = (String)ref.getProperty(SERVLET_PATH_PROP);
+            if(method.equals(serviceMethod) && (servicePath == null || servicePath.startsWith(path))) {
+                selectedService = ref;
+            }
+        }
+        
+        if(selectedService == null) {
+            throw new ServletException("No Servlet service found to handle method " + method + " and path " + path);
+        }
+        
+        final Servlet servlet = (Servlet)bundleContext.getService(selectedService);
+        log.debug("Dispatching to {}", servlet);
+        servlet.service(req, resp);
     }
 }
---
+++ core/src/main/java/ch/x42/osgi/samples/osgi101/core/SampleGetServlet.java
@@ -0,0 +1,27 @@
+package ch.x42.osgi.samples.osgi101.core;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+@SuppressWarnings("serial")
+@Component
+@Service(value=Servlet.class)
+@Property(name=DispatcherServlet.SERVLET_METHOD_PROP, value="GET")
+public class SampleGetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) 
+    throws ServletException, IOException {
+        response.getWriter().println("This is " + getClass().getName());
+        response.getWriter().flush();
+    }
+}
